06dc5b1164785241351394fb65e511fba421ea08
Ryan Yancey <ryancey@zymoresearch.com>
2024-03-12 18:58:53 +0000

    Code formatting start

--- R/csmFinder.R
+++ R/csmFinder.R
@@ -1,10 +1,21 @@
-csmFinder <- function(candidate,data_type='regular',depth=10,distance=0.3,pval=0.05,thread=1){
-  if(data_type=='regular'){
-    model_result <- Nonparametric_Bayesian_clustering(candidate,delta= 0.5,tau = 0.5,nperm = 10000,thread=thread)
-    pCSM <- data.frame(candidate,t(model_result))
-    pCSM <- pCSM[which(pCSM$d>=distance & pCSM$pval<=pval),]
-
-  }else{
-    pCSM <- run.beta.mixture.model(candidate,thread=thread,cell_number=depth,adjusted_pval=pval,distance=distance)
+csmFinder <- function(candidate, data_type = "regular", depth = 10, distance = 0.3, pval = 0.05, thread = 1) {
+  if (data_type == "regular") {
+    model_result <- Nonparametric_Bayesian_clustering(
+      candidate,
+      delta = 0.5,
+      tau = 0.5,
+      nperm = 10000,
+      thread = thread
+    )
+    pCSM <- data.frame(candidate, t(model_result))
+    pCSM <- pCSM[which(pCSM$d >= distance & pCSM$pval <= pval), ]
+  } else {
+    pCSM <- run.beta.mixture.model(
+      candidate,
+      thread = thread,
+      cell_number = depth,
+      adjusted_pval = pval,
+      distance = distance
+    )
   }
 }
